221aefb094907281d0cefda19b2b19581e48129a
taaki <taaki.paul@gmail.com>
2026-06-01 19:37:49 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")"
 
+echo "Creating temporary database file"
 DATABASE=$(mktemp)
-echo "Created temporary file: $DATABASE"
+echo "Successfully created temporary database file: $DATABASE"
 
-sqlite3 "$DATABASE" < setup.sql
 echo "Creating tables in database"
+sqlite3 "$DATABASE" < setup.sql
+echo "Successfully created tables in database"
 
 for DIRECTORY in service website; do
+    echo "Generating ORM code for $DIRECTORY"
     sea-orm-cli generate entity \
         --output-dir ./$DIRECTORY/src/database \
         --with-prelude all-allow-unused-imports \
         --database-url sqlite://"$DATABASE"?mode=ro
+    echo "Successfully generated ORM code for $DIRECTORY"
 done
 
+echo "Deleting temporary database file: $DATABASE"
 rm "$DATABASE"
-echo "Deleting temporary file"
+echo "Successfully deleted temporary database file: $DATABASE"
 
-TIMESTAMP=./service/timestamp.txt
-printf "1985-04-12T23:20:50.52Z" > $TIMESTAMP
-echo "Writing dummy timestamp to $TIMESTAMP"
+echo "Running setup script for service"
+sh ./service/setup.sh
+echo "Successfully ran setup script for service"
